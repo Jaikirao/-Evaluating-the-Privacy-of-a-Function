40eb0de861274d928ee7842c2226c00496efbc46
Jaikirao <48488555+Jaikirao@users.noreply.github.com>
2019-06-22 19:43:46 +0530

    Update Initial and parallel.py

--- Initial and parallel.py
+++ Initial and parallel.py
@@ -1,19 +1,23 @@
-import torch
-# the number of entries in our database
-num_entries = 5000
-db = torch.rand(num_entries) > 0.5
-db
-db, pdbs = create_db_and_parallels(5000)
-def query(db):
-    return db.sum()
-full_db_result = query(db)
-sensitivity = 0
-for pdb in pdbs:
+def sensitivity(query,n_enteries=1000):
+  db, pdbs = create_db_and_parallel(n_enteries)
+  full_db_result = query(db)
+  max_distance = 0
+  for pdb in pdbs:
+    
     pdb_result = query(pdb)
     
     db_distance = torch.abs(pdb_result - full_db_result)
     
-    if(db_distance > sensitivity):
-        sensitivity = db_distance
-sensitivity
+    if(db_distance > max_distance):
+        max_distance = db_distance
+  return max_distance
+
+#now we make a another query fuction
+def query(db):
+    return db.float().mean()
+sensitivity(query)
+
+db, pdbs = create_db_and_parallel(20)
+db
+pdbs
 
